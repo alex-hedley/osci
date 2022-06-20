f8f67de47e4007baf5bee670904c05cafd2e5aaa
Alex Hedley <alex.hedley@opencastsoftware.com>
2022-06-20 14:07:35 +0100

    Initial work on a site

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('myController', function ($scope, $http, $q, $filter) {
+
+    $scope.data = {};
+
+    $scope.init = function () {
+        getData();
+    }
+
+    getData = () =>  {
+        var file = 'data/stats.json';
+
+        $http.get(file)
+        .then(function(response) {
+            // $scope.data = response.data;
+            console.log(response.data);
+            var twentyTwentyTwo = response.data["2022"];
+            const keys = Object.keys(twentyTwentyTwo);
+            console.log(keys);
+            const months = Object.values(twentyTwentyTwo);
+            console.log(months);
+            $scope.data = months;
+        });
+    };
+
+    $scope.init();
+});
