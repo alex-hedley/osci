ddc78c76fb1e97e5952ccd231bd9824144636f18
Alex Hedley <alex.hedley@opencastsoftware.com>
2022-10-03 14:46:16 +0100

    Added chart

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-lightgrey.svg)](https://opensource.org/licenses/MIT)
 
-> Stats
+> Open Source Contributor Index  
+> Welcome to OSCI, where commercial organizations are ranked by the volume of their employees’ year-to-date open source contributions on GitHub
+
+Stats for Opencast Software.
+
+## Website
+
+- [localhost](http://localhost:8000)
+- [deployed](https://alex-hedley.github.io/osci/)
+
+## Links
 
 - [OSCI](https://opensourceindex.io)
 - [GitHub](https://github.com/epam/OSCI)
--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ myApp.controller('myController', function ($scope, $http, $q, $filter) {
 
     $scope.init = function () {
         getData();
+        // generateChart();
     }
 
     getData = () =>  {
@@ -21,8 +22,104 @@ myApp.controller('myController', function ($scope, $http, $q, $filter) {
             const months = Object.values(twentyTwentyTwo);
             // console.log(months);
             $scope.data = months;
+
+            generateChart(keys, months);
         });
     };
+
+    generateChart = (keys, values) => {
+        // Chart.register(ChartDataLabels);
+        // Change default options for ALL charts
+        // Chart.defaults.set('plugins.datalabels', {
+        //     color: '#FE777B'
+        // });
+
+        const options = {
+            plugins: {
+                autocolors: false,
+                datalabels: {
+                    color: '#36A2EB'
+                }
+                // annotation: {
+                //     annotations: {
+                //         line1: {
+                //             type: 'line',
+                //             yMin: 60,
+                //             yMax: 60,
+                //             borderColor: 'rgb(255, 99, 132)',
+                //             borderWidth: 2,
+                //         }
+                //     }
+                // }
+            }
+        };
+
+        var positionChange = values.map(v => v.positionChange)
+        var activeContributors = values.map(v => v.activeContributors)
+        var activeContributorsChange = values.map(v => v.activeContributorsChange)
+        var totalCommunity = values.map(v => v.totalCommunity)
+        var totalCommunityChange = values.map(v => v.totalCommunityChange)
+        var position = values.map(v => v.position)
+
+        const config = {
+            type: 'line',
+            data: {
+                labels: keys,
+                datasets: [
+                    {
+                        label: 'Position Change',
+                        data: positionChange,
+                        fill: false,
+                        borderColor: 'red',
+                        tension: 0.1,
+                        datalabels: {
+                            color: '#FFCE56'
+                        }
+                    },
+                    {
+                        label: 'Active Contributors',
+                        data: activeContributors,
+                        fill: false,
+                        borderColor: 'green',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Active Contributors Change',
+                        data: activeContributorsChange,
+                        fill: false,
+                        borderColor: 'blue',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Total Community',
+                        data: totalCommunity,
+                        fill: false,
+                        borderColor: 'yellow',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Total Community Change',
+                        data: totalCommunityChange,
+                        fill: false,
+                        borderColor: 'orange',
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Position',
+                        data: position,
+                        fill: false,
+                        borderColor: 'brown',
+                        tension: 0.1
+                    }
+                ]
+            },
+            options
+        };
+
+        let ctx = $("#myChart");
+
+        let lineGraph = new Chart(ctx, config);
+    }
 
     $scope.init();
 });
